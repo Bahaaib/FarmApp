ae52292d2297923c0849f927ad05538f8031d46f
Bahaaib <Bahaasco@gmail.com>
2019-05-12 02:39:59 +0200

    created registered devices DB

--- app/src/main/java/com/ets/android_team/farmApp/HomeActivity.java
+++ app/src/main/java/com/ets/android_team/farmApp/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.ets.android_team.farmApp;
 
 import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
@@ -12,16 +14,30 @@ import android.view.View;
 import android.widget.Button;
 
 import com.ets.android_team.farmApp.Dialogs.InfoDialog;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     private final String INFO_TAG = "infoDialog";
+    private static String DEVICE_ID;
+    private final String DEVICES_DB = "devices";
+
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private DialogFragment infoDialog;
     private Button infoBtn;
+    //Firebase DB
+    private FirebaseDatabase database;
+    private DatabaseReference mRef;
+    private ArrayList<String> devicesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,15 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         FirebaseMessaging.getInstance().subscribeToTopic("messages");
+
+        //Firebase DB
+        database = FirebaseDatabase.getInstance();
+        mRef = database.getReference();
+        devicesList = new ArrayList<>();
+
+        //Fetching Device identifier ID
+        DEVICE_ID = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
+        callDevicesDatabase();
 
 
         toolbar = findViewById(R.id.toolbar);
@@ -82,8 +107,6 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
-
-
     }
 
     void showInfoDialog() {
@@ -97,6 +120,48 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+
+    private void callDevicesDatabase() {
+        mRef.child(DEVICES_DB).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Reset List of products
+                devicesList.clear();
+                fetchData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void fetchData(DataSnapshot dataSnapshot) {
+        for (DataSnapshot db : dataSnapshot.getChildren()) {
+            String device = db.getKey();
+            devicesList.add(device);
+        }
+
+        if (!deviceIsRegistered()) {
+            registerDevice();
+        }
+    }
+
+    private boolean deviceIsRegistered() {
+        for (String deviceId : devicesList) {
+            if (deviceId.equals(DEVICE_ID)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //Push device ID to DB
+    private void registerDevice() {
+        mRef.child(DEVICES_DB).child(DEVICE_ID).setValue(0);
+    }
 
 
 }
